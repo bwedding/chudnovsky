861a23a2fffb5f1d892c202adc1c92fad1dccb70
Bwedding <therealbruce@gmail.com>
2024-03-05 23:52:00 +0100

    Added a MSVC solution. converted threads to parallel foreach. Added a "theoretically" faster pow_3k

--- chudnovsky.cpp
+++ chudnovsky.cpp
@@ -15,30 +15,37 @@
 #include <vector>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/mpfr.hpp>
+#include <algorithm>
+#include <iostream>
+#include <execution>
 
 using boost::multiprecision::cpp_int;
 
 static const int log2n = 4;
 
-cpp_int factorial(const cpp_int& num) {
+cpp_int factorial(const cpp_int& num) 
+{
     cpp_int fact = 1;
     for(cpp_int i = 1; i <= num; ++i)
         fact *= i;
     return fact;
 }
 
-cpp_int numerator(const cpp_int& k) {
+cpp_int numerator(const cpp_int& k) 
+{
     auto six_k_fact = factorial(6*k);
     
     return (k & 1 ? -1 : 1) * six_k_fact * (545140134*k + 13591409);
 }
 
-cpp_int denominator_a(const cpp_int& k) {
+cpp_int denominator_a(const cpp_int& k) 
+{
     auto factorial_k = factorial(k);
     return factorial(3*k) * factorial_k * factorial_k * factorial_k;
 }
 
-cpp_int pow_3k(int64_t k) {
+cpp_int pow_3k(int64_t k) 
+{
     cpp_int base = 640320;
     cpp_int ret = 1;
     int64_t exponent = 3 * k;
@@ -49,14 +56,42 @@ cpp_int pow_3k(int64_t k) {
     return ret;
 }
 
-int calcPrecision(int num_terms) {
+cpp_int newpow_3k(int64_t k)
+{
+    cpp_int base = 640320;
+    cpp_int ret = 1;
+    int64_t exponent = 3 * k;
+
+    if (exponent < 0)
+    {
+        base = 1 / base;
+        exponent = -exponent;
+    }
+
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            // If exponent is odd
+            ret *= base;
+        }
+        base *= base; // Square the base
+        exponent /= 2; // Divide exponent by 2
+    }
+    return ret;
+
+}
+
+int calcPrecision(int num_terms) 
+{
     const int PLACES_PER_TERM = 15;
 
     return num_terms * PLACES_PER_TERM;
 }
 
 // Function to calculate multiple of sigma series with required precision
-boost::multiprecision::mpfr_float calcConstant(int precision){
+boost::multiprecision::mpfr_float calcConstant(int precision)
+{
     using boost::multiprecision::mpfr_float;
     mpfr_float::default_precision(precision * log2n);
     mpfr_float numerator = 1;
@@ -67,8 +102,10 @@ boost::multiprecision::mpfr_float calcConstant(int precision){
 }
 
 
-int main(int argc, char* argv[]) {
-    if(argc != 2) {
+int main(int argc, char* argv[]) 
+{
+    if(argc != 2) 
+    {
         std::cerr << "Usage: " << argv[0] << " number_of_terms\n";
         return 1;
     }
@@ -76,40 +113,46 @@ int main(int argc, char* argv[]) {
     try {
         int64_t num_terms = std::stoi(argv[1]);
         
-        if (num_terms <= 0) {
+        if (num_terms <= 0) 
+        {
             throw std::invalid_argument("Number of terms must be greater than zero.");
         }
- 
+        using boost::multiprecision::mpfr_float;
+
+        mpfr_float pi;
         int precision = calcPrecision(num_terms);
 
-        using boost::multiprecision::mpfr_float;
-        
         mpfr_float::default_precision(precision * log2n);
         mpfr_float constant = calcConstant(precision);
         mpfr_float pi_inverse = 0;
-          
-        std::mutex mtx;
-        unsigned num_threads = std::thread::hardware_concurrency();
-        std::vector<std::thread> threads(num_threads);
 
-        for(unsigned i = 0; i < num_threads; ++i) {
-            threads[i] = std::thread([&, i] {
-                for(int64_t k = i; k < num_terms; k += num_threads) {
-                    mpfr_float temp = mpfr_float(numerator(k)) / mpfr_float(denominator_a(k) * pow_3k(k));
-                    mtx.lock(); 
-                    pi_inverse += temp;
-                    mtx.unlock(); 
-                }
+        unsigned num_threads = std::thread::hardware_concurrency();
+
+        std::vector<mpfr_float> partial_sums(num_threads);
+            
+        std::for_each(std::execution::par_unseq, partial_sums.begin(), partial_sums.end(), [&](mpfr_float& local_sum) 
+            {
+            auto thread_id = &local_sum - &partial_sums[0];
+            for (int64_t k = thread_id; k < num_terms; k += num_threads) 
+            {
+                mpfr_float temp = mpfr_float(numerator(k)) / mpfr_float(denominator_a(k) * newpow_3k(k));
+                local_sum += temp;
+            }
             });
+
+        // Combine the partial sums
+        for (const auto& sum : partial_sums) 
+        {
+            pi_inverse += sum;
         }
 
-        for(auto& th : threads)
-            th.join();
+        pi = mpfr_float(1) / (pi_inverse * constant);
 
-        mpfr_float pi = mpfr_float(1)/(pi_inverse * constant);
-        
         std::cout << std::setprecision(precision) << pi << "\n";
-    } catch(std::exception& e){
+
+    } 
+    catch(std::exception& e)
+    {
         std::cerr << "Error: " << e.what() << "\n";
         return 1;
     }
